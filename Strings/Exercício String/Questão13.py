25c8bf1256760e66aaeab7494d96c41d3d6c4311
Marian4 <Mraes.Mariana@gmail.com>
2016-10-13 00:02:26 -0300

    Update Questão13.py

--- Strings/Exercício String/Questão13.py
+++ Strings/Exercício String/Questão13.py
@@ -2,33 +2,43 @@ import random
 
 print("Bem vindo ao Jogo da palavra embaralhada!")
 
-jogar = str(input("jogar? Sim/Não."))
+jogar = str(input("jogar? Sim/Não/Adicionar palavra."))
+
+a = 'Otorrinolaringologista'
+
+b = 'Paralelepipedo'
+
+c = 'Caminhão'
+    
+d = 'Filtro'
+
+e = 'Facebook'
+
+f = 'Banheiro'
+
+g = 'Torta'
+
+h = 'Chocolate'
+
+i = 'Júpiter'
+
+j = 'Segredo'
+
+k = 'Guardanapo'
+
+lista = [a,b,c,d,e,f,g,h,i,j,k]
+
+while jogar == 'Adicionar palavra' or jogar == 'adicionar palavra':
+	
+        digitar = input("digite a palavra aqui:")
+	
+        lista.append(digitar)
+	
+        print("\nObrigado pela colaboração.:D\n")
+	
+        jogar = str(input("jogar? Sim/Não/Adicionar palavra:\n"))
 
 while jogar == 'Sim' or jogar == 'sim':
-
-	a = 'Otorrinolaringologista'
-
-	b = 'Paralelepipedo'
-
-	c = 'Caminhão'
-	
-	d = 'Filtro'
-
-	e = 'Facebook'
-
-	f = 'Banheiro'
-
-	g = 'Torta'
-
-	h = 'Chocolate'
-
-	i = 'Júpiter'
-
-	j = 'Segredo'
-
-	k = 'Guardanapo'
-
-	lista = [a,b,c,d,e,f,g,h,i,j,k]
 
 	lista2 = []
 
@@ -38,30 +48,44 @@ while jogar == 'Sim' or jogar == 'sim':
 
 		lista2.append(i)
 
-	random.shuffle(lista2)
-
-	for j in range(len(palavra)):
-
-		print(lista2[j],end=' ')
+    	random.shuffle(lista2)
 
 	print()
 
-	resposta = input("\nQual é a palavra?\n")
+	for j in range(len(palavra)):
 
-	if resposta == palavra:
+        	print(lista2[j],end=' ')
 
-		print("\nParabéns, você acertou!!")
+    	print()
 
-		jogar = str(input("\njogar novamente? Sim/Não.\n"))
+    	resposta = input("\nQual é a palavra?\n")
 
-	else:
+    	if resposta == palavra:
 
-		print("\nQue pena, você errou.\n")
+        	print("\nParabéns, você acertou!!:)")
 
-		print("A resposta certa era",palavra)
+        	jogar = str(input("\njogar novamente? Sim/Não/Adicionar palavra.\n"))
 
-		jogar = str(input("\njogar novamente? Sim/Não.\n"))
+    	else:
 
+        	print("\nQue pena, você errou.:(\n")
+
+       		print("A resposta certa era",palavra)
+
+        	jogar = str(input("\njogar novamente? Sim/Não/Adicionar palavra.\n"))
+		
+    	while jogar == 'Adicionar palavra' or jogar == 'adicionar palavra':
+		
+        	digitar = input("digite a palavra aqui:")
+		
+        	lista.append(digitar)
+		
+        	print("\nObrigado pela colaboração.:D\n")
+		
+        	jogar = str(input("jogar? Sim/Não/Adicionar palavra:"))
+		
 if jogar == 'não' or jogar == 'Não':
-	print("Obrigado por jogar")
+	
+    	print("obrigado por jogar")
+	
 print("Programa encerrado.")
